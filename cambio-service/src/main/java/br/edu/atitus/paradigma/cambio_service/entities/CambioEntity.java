56f71ac7921a0337f19cb418a25b8ac0f184930d
bernardosfz <bernardofattini@gmail.com>
2024-10-24 19:27:53 -0300

    Correções no cambio-service; Clients e Controllers;

--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/entities/CambioEntity.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/entities/CambioEntity.java
@@ -8,57 +8,69 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name="cambio")
+@Table(name = "cambio")
 public class CambioEntity {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
+	
 	private String origem;
 	private String destino;
 	private double fator;
 	
-	@Transient 
+	@Transient
 	private String ambiente;
-	
-	@Transient 
+	@Transient
 	private double valorConvertido;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getOrigem() {
 		return origem;
 	}
+
 	public void setOrigem(String origem) {
 		this.origem = origem;
 	}
+
 	public String getDestino() {
 		return destino;
 	}
+
 	public void setDestino(String destino) {
 		this.destino = destino;
 	}
+
 	public double getFator() {
 		return fator;
 	}
+
 	public void setFator(double fator) {
 		this.fator = fator;
 	}
+
 	public String getAmbiente() {
 		return ambiente;
 	}
+
 	public void setAmbiente(String ambiente) {
 		this.ambiente = ambiente;
 	}
+
 	public double getValorConvertido() {
 		return valorConvertido;
 	}
+
 	public void setValorConvertido(double valorConvertido) {
 		this.valorConvertido = valorConvertido;
 	}
+
+	
 }
--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/repositories/CambioRepository.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/repositories/CambioRepository.java
@@ -11,4 +11,5 @@ import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
 public interface CambioRepository extends JpaRepository<CambioEntity, Integer>{
 	
 	Optional<CambioEntity> findByOrigemAndDestino(String origem, String destino);
+
 }
